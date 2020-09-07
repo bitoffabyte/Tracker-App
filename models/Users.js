c504b005e6e360811694dfaf225fc2021b7c9b14
R Narayan <nr.rnarayan@gmail.com>
2020-09-07 17:43:44 +0530

    s

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+const mongo = require('mongoose');
+
+const UserSchema = new mongo.Mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+modules.exports = User = mongoose.model('user', UserSchema);
